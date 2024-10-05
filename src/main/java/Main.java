25e690a19c10dc5d2789bf213197b8ec7f1990ea
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2024-10-05 19:04:37 +0200

    Commit inicial - loggin ajustar disposiciones y tamaños

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import views.Loggin;
+
+public class Main {
+    public static void main(String[] args) {
+        Loggin l = new Loggin();
+
+        l.setVisible(true);
+    }
+}
--- src/main/java/views/Loggin.java
+++ src/main/java/views/Loggin.java
@@ -1,9 +1,53 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Loggin {
-    private JPanel panel1;
+public class Loggin extends JFrame {
+    private JPanel ventana;
     private JPasswordField passwordField1;
     private JTextField textField1;
+    private JButton iniciarSesionButton;
+    private JButton cerrarAppButton;
+    private JPanel userpass;
+    private JPanel button;
+
+
+    public Loggin() {
+        setContentPane(ventana);
+        setTitle("loggin");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300,400);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+
+        iniciarSesionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //
+            }
+        });
+
+
+        cerrarAppButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                closeButton();
+            }
+        });
+
+
+    }
+
+    private void closeButton(){
+        dispose();
+    }
+
+
+
+
+
 }
