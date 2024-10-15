d9e016c8983aeb53841084c8d65e547c75721e12
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2024-10-15 19:26:41 +0200

    Commit ventanaDialogo radioButton

--- src/main/java/reto/views/AddCopia.java
+++ src/main/java/reto/views/AddCopia.java
@@ -1,13 +1,19 @@
 package reto.views;
 
+import reto.JdbcUtils;
+import reto.dao.CopiaDAO;
+import reto.models.Copia;
+
 import javax.swing.*;
+
+import static reto.Session.copySelected;
 
 public class AddCopia extends JDialog {
 
     private JPanel ventanaAdd;
     private JPanel DatosAdd;
     private JComboBox soporteCombo;
-    private JComboBox soportePelicula;
+    private JComboBox peliculaCombo;
     private JRadioButton buenoRadioButton;
     private JRadioButton dañadoRadioButton;
     private JButton btnGuardar;
@@ -23,6 +29,38 @@ public class AddCopia extends JDialog {
         setResizable(false);
         //pack();
 
+        CopiaDAO cDAO = new CopiaDAO(JdbcUtils.getCon());
+        Copia nuevaCopia = new Copia();
+
+
+
+        var opcionesSoporte = new DefaultComboBoxModel<String>();
+        soporteCombo.setModel(opcionesSoporte);
+        opcionesSoporte.addElement("");
+        opcionesSoporte.addElement("DVD");
+        opcionesSoporte.addElement("Blu-ray");
+        //setear el soporte en la nueva copia
+        nuevaCopia.setSoporte(opcionesSoporte.toString());
+
+        //setear peliculaCombo
+
+
+
+
+
+        buenoRadioButton.addActionListener( e ->{
+            if (buenoRadioButton.isSelected()){
+                dañadoRadioButton.setSelected(false);
+                nuevaCopia.setEstado("bueno");
+            }
+        });
+
+        dañadoRadioButton.addActionListener( e ->{
+            if (dañadoRadioButton.isSelected()){
+                buenoRadioButton.setSelected(false);
+                nuevaCopia.setEstado("dañado");
+            }
+        });
 
     }
 }
